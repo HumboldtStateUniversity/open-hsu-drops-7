c5608f1fef9eb38295109dc26286874f7219687e
Matt Hodgson <mmh61@humboldt.edu>
2015-12-01 11:19:10 -0800

    Latest theme updates

--- profiles/openhsu/themes/hsu_kalatheme/templates/sections/hsu-site-header.tpl.php
+++ profiles/openhsu/themes/hsu_kalatheme/templates/sections/hsu-site-header.tpl.php
@@ -1,49 +1,31 @@
-<header role="banner" class="masthead">
- <div class="container">
-  <div class="row">
-    <div class="col-sm-6">
+<header role="banner">
+  <div class="hsu-header">
+    <div class="container department-brand">
     <?php if ($logo): ?>
-      <div class='brand pull-left'>
-
-        <p class="hsumark brand pull-left">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-          <span class="element-invisible">Humboldt State University</span>
-          
-        </p>
-
-
-      </div>
+      <div class="logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            </a>
+        </div>
     <?php endif; ?>
 
-    </div><!-- /.col-sm-6 -->
+    <?php if ($site_name): ?>
+      <h1 <?php if ($hide_site_name) { print ' class="element-invisible"'; } ?> <?php {print ' class="kilo"'; }?>>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+      </h1>  
+    <?php endif; ?>
 
-    <div class="col-sm-6">
-      <?php if ($site_slogan): ?>
-        <div id="site-slogan" <?php if ($hide_site_slogan) { print 'class="element-invisible"'; } ?>>
-          <p class="lead text-right">
-            <?php print $site_slogan; ?>
-          </p>
-        </div>
-      <?php endif; ?>
-    </div><!-- /.col-sm-6 -->
-
-  </div><!--/.row-->
-
- </div><!--/.container-->
-
-  <?php if ($hsu_banner && $hsu_header): ?>
-    <div class="row">
-      <div class="hsu-header">
-        <?php print render($hsu_header_image); ?>
-        <?php if ($site_name): ?>
-          <div class="container">
-            <span class="hsu-header-site-name"><?php print $site_name; ?></span>
-          </div>
-        <?php endif; ?>
+    <?php if ($site_slogan): ?>
+      <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+        <p class="department-slogan"><?php print $site_slogan; ?></p>
       </div>
-    </div>
-  <?php endif; ?>
 
+    <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
+
+   <?php if ($hsu_header): ?>
+      <div class="header-image">
+        <?php print render($hsu_header_image); ?>
+      </div>
+    <?php endif; ?>
+  </div>
 </header>
