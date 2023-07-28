0586882e407375ddb9d4455f8cf3b43263e15fbe
Matt Hodgson <mmh61@humboldt.edu>
2023-07-28 08:33:11 -0700

    Update favicons to new mark

--- profiles/openhsu/themes/hsu_kalatheme/templates/core/html.tpl.php
+++ profiles/openhsu/themes/hsu_kalatheme/templates/core/html.tpl.php
@@ -67,10 +67,10 @@
     })(window, document, 'script', 'dataLayer', 'GTM-PJJ2KH2');
   </script>
   <!-- End Google Tag Manager -->
-  <!-- my fancy new favicon code -->
-  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+  <!-- Update favicons to new mark -->
+  <link rel="icon" href="/favicons/favicon.ico" sizes="32x32">
+  <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png">
   <link rel="manifest" href="/favicons/site.webmanifest">
   <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
   <link rel="shortcut icon" href="/favicons/favicon.ico">
