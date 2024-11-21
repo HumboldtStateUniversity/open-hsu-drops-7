0587f2c12f31c582cba45f2f2a188b16bef6b669
Matt Hodgson <mmh61@humboldt.edu>
2024-11-21 09:35:51 -0800

    Update to 1.47 release, See Changelog.txt

--- profiles/openhsu/themes/hsu_kalatheme/templates/core/html.tpl.php
+++ profiles/openhsu/themes/hsu_kalatheme/templates/core/html.tpl.php
@@ -43,7 +43,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
